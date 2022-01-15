92ab3310c350ecbcc826c76c2a718374b7a32a31
RaVoTTi <valenravo@gmail.com>
2022-01-15 11:27:30 -0300

    App React

--- app.js
+++ app.js
@@ -7,33 +7,33 @@ const port = process.env.PORT
 
 
 
-hbs.registerPartials(__dirname + '/views/partials',  (err) => {});
 
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials',  (err) => {});
 
 app.use(express.static('public'))
 
-app.get('/',  (req, res) => {
-    res.render('home', {
-        nombre: 'Valentin Ravotti',
-        titulo: 'Curso Node'
-    })
-})
-app.get('/generic',(req, res) => {
-    res.render('generic', {
-      nombre: 'Valentin Ravotti',
-      titulo: 'Curso Node'
-  })
-  })
-  app.get('/elements',(req, res) => {
-    res.render('elements', {
-      nombre: 'Valentin Ravotti',
-      titulo: 'Curso Node'
-  })
-  })
+// app.get('/',  (req, res) => {
+//     res.render('home', {
+//         nombre: 'Valentin Ravotti',
+//         titulo: 'Curso Node'
+//     })
+// })
+// app.get('/generic',(req, res) => {
+//     res.render('generic', {
+//       nombre: 'Valentin Ravotti',
+//       titulo: 'Curso Node'
+//   })
+//   })
+//   app.get('/elements',(req, res) => {
+//     res.render('elements', {
+//       nombre: 'Valentin Ravotti',
+//       titulo: 'Curso Node'
+//   })
+//   })
 
 app.get('*',  (req, res) => {
-    res.send('/public/404.html')
+    res.sendFile(__dirname + '/public/index.html')
 
 })
 
